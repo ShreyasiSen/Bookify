ccfe4913d56b4caee14213812da4e34577e7abdc
ShreyasiSen <shreyasisen05@gmail.com>
2025-05-15 11:20:40 +0530

    updated geosearch

--- src/components/geo/nearby.jsx
+++ src/components/geo/nearby.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import Navbar from '../navbar/Navbar';
 import { useNavigate } from 'react-router-dom';
